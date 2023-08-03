71a591d1f73f14a1da5c4dfbc3686e82df35f09e
yogesh <yogirao8440@gmail.com>
2023-08-03 20:14:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import Navbar from './components/Navbar';
 function App() {
  
   return (
-    <div className="App">
+    <div className="App" >
       <Navbar/>
       <BrowserRouter>
       <Routes>
         <Route exact path='/student/:verification' element={<Home/>}/>
-        <Route exact path='/' element={<Home/>} />
+        <Route  path='/' element={<Home/>} />
       </Routes>
      
       </BrowserRouter>
